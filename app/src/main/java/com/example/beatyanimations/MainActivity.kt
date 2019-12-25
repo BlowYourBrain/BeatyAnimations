f7ae6bc8afbf9d1b369edc81f7481c0f413501c6
Evgeny <fetel@yandex.ru>
2019-12-25 19:49:23 +0300

    init

---
+++ app/src/main/java/com/example/beatyanimations/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.beatyanimations
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.header_layout.*
+
+class MainActivity : AppCompatActivity() {
+
+    var counter: Int = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
